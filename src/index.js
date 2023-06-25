7e50123076406a82f71dd14da41a49c395acd64e
Nick Garner <nickgcs13@gmail.com>
2023-06-25 15:47:15 -0500

    movie

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-//import './index.css';
-//import App from './App';
+import './index.css';
+import App from './App';
 import StarRating from './StarRating';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/*<App /> */}
-    <StarRating maxRating={5} />
+    <App />
+   {/* <StarRating
+      maxRating={5} 
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <StarRating size={24} color='red' className='test' defaultRating={3}/>
+
+<Test /> */}
   </React.StrictMode>
 );
